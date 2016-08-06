842c4483e1063bdf475f459acb37e72588eb4c6b
Kyle R <KyleRichVA@gmail.com>
2016-08-06 15:24:35 -0700

    added test for create_url

--- ticketpy/connection.py
+++ ticketpy/connection.py
@@ -3,7 +3,7 @@ Script containing the Connection Class.
 
 Connections are used to establish a reusable connection with the
 Ticketmaster API. These Connections are used to make all API
-requests to Ticketmaster.
+requests to Ticketmaster(Refered to as 'TM' in comments).
 
 Written by Kyle Richardson
 
@@ -12,12 +12,15 @@ http://developer.ticketmaster.com
 """
 
 class Connection(object):
-    """Connection class used to make API queries to Ticketmaster
-
-    Parameters:
-    api_key(string): Your Application API key provided by Ticketmaster
-    version(int): Specify the version of TM API to be used.
-        defaults to most current verison (2.0 as of 8/6/16)"""
+    """Connection class used to make API queries to Ticketmaster"""
     def __init__(self, api_key, version=2):
+        """Initiation method for Connection Class
+        Parameters:
+        api_key(string): Your Application API key provided by Ticketmaster
+        version(int): Specify the version of TM API to be used.
+            defaults to most current verison (2.0 as of 8/6/16)"""
         self.api_key = api_key
         self.version = version
+
+    def create_url(self, api_cat):
+        """Creates the url for TM API requests."""
--- ticketpy/tests/test_connection.py
+++ ticketpy/tests/test_connection.py
@@ -1,5 +1,5 @@
 import pytest
-from ticketpy import connection
+from ticketpy.connection import Connection
 
 """
 Testing file for Connection Class and its modules
@@ -7,3 +7,21 @@ Testing file for Connection Class and its modules
 Written By Kyle Richardson
 """
 
+
+@pytest.fixture()
+def test_connection_no_ver():
+    return Connection("thisismyfakeapikey")
+
+
+@pytest.fixture()
+def test_connection_ver():
+    return Connection("thisismyfakeapikey", 2)
+
+
+def test_create_url(test_connection_no_ver, test_connection_ver):
+    """Tests create_url method combines strings together
+    correctly."""
+    good_url = ("https://app.ticketmaster.com/discovery/v2/"
+                "events.json?apikey=thisismyfakeapikey")
+    assert test_connection_no_ver.create_url("discovery") == good_url
+    assert test_connection_ver.create_url("discovery") == good_url
